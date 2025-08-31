f6be8f1aae7b2ac4463c7af4b0c3ad0c115f764f
AleksandrKompanetc <moneymacker7@gmail.com>
2025-08-31 23:58:56 +0300

    changed DEFAULT_TODO_LIST

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './App.css';
 
 const DEFAULT_TODO_LIST = [
-  { id: 1, name: '', description: '', checked: false}, 
-  { id: 1, name: '', description: '', checked: false},
-  { id: 1, name: '', description: '', checked: false},
+  { id: 1, name: 'task 1', description: 'description 1', checked: false}, 
+  { id: 2, name: 'task 2', description: 'description 2', checked: false},
+  { id: 3, name: 'task 3', description: 'description 3', checked: false},
 ]
 
 export const App = () => {
