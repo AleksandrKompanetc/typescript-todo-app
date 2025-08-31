4cc5eec1572955476af7b75aa38f03e009bf89c2
AleksandrKompanetc <moneymacker7@gmail.com>
2025-08-31 23:56:58 +0300

    created DEFAULT_TODO_LIST

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
 
+const DEFAULT_TODO_LIST = [
+  { id: 1, name: '', description: '', checked: false}, 
+  { id: 1, name: '', description: '', checked: false},
+  { id: 1, name: '', description: '', checked: false},
+]
+
 export const App = () => {
   return (
     <div className="app-container">
