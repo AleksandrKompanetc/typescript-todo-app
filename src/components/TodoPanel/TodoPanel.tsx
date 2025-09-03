f5ab55839253008c5fe364211b7ab2393dbcabdd
AleksandrKompanetc <moneymacker7@gmail.com>
2025-09-03 15:14:51 +0300

    added type for TodoPanel

--- src/components/TodoPanel/TodoPanel.tsx
+++ src/components/TodoPanel/TodoPanel.tsx
@@ -1,42 +1,46 @@
 import React, { useState } from 'react';
 import styles from './TodoPanel.module.css';
 
+interface TodoPanelProps {
+  onAddTodo: () => void;
+}
+
 const DEFAULT_TODO = {
   name: '',
   description: ''
 }
 
-export const TodoPanel = () => {
+export const TodoPanel: React.FC<TodoPanelProps> = () => {
   const [todo, setTodo] = useState({ name: '', description: '' });
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  const { name, value } = event.target;
-  setTodo({...todo, [name]: value });
-}
+    const { name, value } = event.target;
+    setTodo({ ...todo, [name]: value });
+  }
 
   return (
-    <div className={styles.todopanel_container}>
+    <div className={styles.todo_panel_container}>
       <div>
         <label htmlFor="Name">
           <div>Name</div>
-          <input 
-            type="text" 
-            id='name' 
-            value={todo.name} 
-            name='name' 
-            onChange={onChange} 
+          <input
+            type="text"
+            id='name'
+            value={todo.name}
+            name='name'
+            onChange={onChange}
           />
         </label>
       </div>
       <div>
         <label htmlFor="Description">
           <div>Description</div>
-          <input 
-            type="text" 
-            id='description' 
-            value={todo.description} 
-            name='description' 
-            onChange={onChange} 
+          <input
+            type="text"
+            id='description'
+            value={todo.description}
+            name='description'
+            onChange={onChange}
           />
         </label>
       </div>
