907956372cdb1e6f65b006026925d8589893c211
AleksandrKompanetc <moneymacker7@gmail.com>
2025-09-04 23:49:10 +0300

    added interface ButtonProps

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 import styles from './Button.module.css';
 
-
+interface ButtonProps extends React.ComponentPropsWithRef<'button'> {
+  color: 'orange' | 'blue' | 'red';
+}
 
 export const Button = () => {
   return (
