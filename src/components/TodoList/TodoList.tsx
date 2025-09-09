2eabbeb7865793e259cd32787cd6165a310f735c
AleksandrKompanetc <moneymacker7@gmail.com>
2025-09-09 23:40:04 +0300

    changed TodoList page

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -6,6 +6,10 @@ interface TodoListProps {
 
 export const TodoList: React.FC<TodoListProps> = ({todos}) => {
   return (
-
+    <div>
+      {todos.map((todo) => (
+        <div>{todo.name}</div>
+      ))}
+    </div>
   )
 }
