e388f08c45822e4848dca60ee3aca004bbd563e8
AleksandrKompanetc <moneymacker7@gmail.com>
2025-09-09 23:55:40 +0300

    added types for TodoItem

--- src/components/TodoList/TodoItem/TodoItem.tsx
+++ src/components/TodoList/TodoItem/TodoItem.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-export const TodoItem = ({ todo }) => {
+interface TodoItemProps {
+  todo: Todo;
+}
+
+export const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
   console.log('@', todo)
   return (
     <div>{todo.name}</div>
