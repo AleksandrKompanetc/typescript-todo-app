7d8a2088f59ebd1bf12edd0d9ccc0b5e5e34d6a5
AleksandrKompanetc <moneymacker7@gmail.com>
2025-09-16 00:18:49 +0300

    changed TodoList and styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,25 @@ export default function App() {
   return (
     <div className='App'>
       <TodoList />
+      <TodoList />
+      <TodoList />
+    </div>
+  )
+}
+
+function TodoList() {
+  return (
+    <div>
+      <h3>What to learn?</h3>
+      <div>
+        <input type="text" />
+        <button>+</button>
+      </div>
+      <ul>
+        <li><input type="checkbox" checked={true} /><span>HTML&CSS</span></li>
+        <li><input type="checkbox" checked={true} /><span>JS</span></li>
+        <li><input type="checkbox" checked={false} /><span>React</span></li>
+      </ul>
     </div>
   )
 }
