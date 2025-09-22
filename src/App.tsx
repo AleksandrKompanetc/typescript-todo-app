0da6f063c0c060ec6e052989471d93bec39eade8
AleksandrKompanetc <moneymacker7@gmail.com>
2025-09-22 22:58:25 +0300

    created todolists

--- src/App.tsx
+++ src/App.tsx
@@ -66,6 +66,20 @@ export default function App() {
     { id: v1(), title: 'What to buy', filter: 'completed' }
   ])
 
+  let [allTasks, setAllTasks] = useState({
+    todolistId1: [
+      {id: v1(), title: 'HTML&CSS', isDone: true},
+      {id: v1(), title: 'JS', isDone: true},
+      {id: v1(), title: 'ReactJS', isDone: false},
+      {id: v1(), title: 'Rest API', isDone: false},
+      {id: v1(), title: 'GraphQL', isDone: false},
+    ],
+    todolistId2: [
+      {id: v1(), title: 'Book', isDone: false},
+      {id: v1(), title: 'Milk', isDone: true},
+    ],
+  })
+ 
   return (
     <div className='App'>
       {
