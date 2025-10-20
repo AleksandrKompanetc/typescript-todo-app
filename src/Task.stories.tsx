6755b0aedbbde4e188f75da9b5c70f728ecb157f
AleksandrKompanetc <moneymacker7@gmail.com>
2025-10-20 23:24:44 +0300

    added tasks for Task story

--- src/Task.stories.tsx
+++ src/Task.stories.tsx
@@ -5,10 +5,25 @@ export default {
   component: Task
 }
 
+const changeTaskStatusCallback = (taskId: string, isDone: boolean) => {
+
+}
+
 export const TaskBaseExample = (props: any) => {
   return <>
     <Task 
       task={{ id: '1', isDone: true, title: 'CSS' }}
+      changeTaskStatus={changeTaskStatusCallback}
+      changeTaskTitle={() => {}}
+      removeTask={() => {}}
+      todolistId={'todolistId1'}
+    />
+    <Task 
+      task={{ id: '2', isDone: false, title: 'JS' }}
+      changeTaskStatus={changeTaskStatusCallback}
+      changeTaskTitle={() => {}}
+      removeTask={() => {}}
+      todolistId={'todolistId1'}
     />
   </>
 }
