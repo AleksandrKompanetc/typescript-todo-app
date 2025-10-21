d170cf674a2fd42ee363c73b3740bd816c80b3a1
AleksandrKompanetc <moneymacker7@gmail.com>
2025-10-21 15:21:54 +0300

    created decorator ReduxStoreProviderDecorator

---
+++ src/stories/ReduxStoreProviderDecorator.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {store} from '../state/store';
+import {Provider} from 'react-redux';
+
+export const ReduxStoreProviderDecorator = (storyFn: any) => {
+  return (
+    <Provider store={store}>
+      {storyFn()}
+    </Provider>
+  )
+}
