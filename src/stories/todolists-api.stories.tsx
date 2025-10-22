62a1279e9ada8d61c60969239e60708d81bc5ddb
AleksandrKompanetc <moneymacker7@gmail.com>
2025-10-22 12:05:31 +0300

    added UpdateTodolistTitle request

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -2,7 +2,10 @@ import { useState, useEffect } from "react"
 import axios from 'axios'
 
 const settings = {
-  withCredentials: true
+  withCredentials: true,
+  headers: {
+    'API-KEY': ''
+  }
 }
 
 export const GetTodolists = () => {
@@ -22,7 +25,33 @@ export const CreateTodolist = () => {
   const [state, setState] = useState<any>(null)
 
   useEffect(() => {
-    axios.post('', {}, settings)
+    axios.post('', { title: 'New todolist' }, settings)
+      .then((res) => {
+        setState(res.data)
+      })
+  }, [])
+
+  return <div>{JSON.stringify(state)}</div>
+}
+
+export const DeleteTodolist = () => {
+  const [state, setState] = useState<any>(null)
+
+  useEffect(() => {
+    axios.delete('', settings)
+      .then((res) => {
+        setState(res.data)
+      })
+  }, [])
+
+  return <div>{JSON.stringify(state)}</div>
+}
+
+export const UpdateTodolistTitle = () => {
+  const [state, setState] = useState<any>(null)
+
+  useEffect(() => {
+    axios.put('', {title: 'Yoyo'}, settings)
       .then((res) => {
         setState(res.data)
       })
